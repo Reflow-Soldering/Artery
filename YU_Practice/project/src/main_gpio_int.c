c87efbc6ae732875768d578f100003d0f20aec37
Reflow-Soldering <reflow_soldering@naver.com>
2025-06-24 11:12:47 +0900

    GPIO EXINT
    GPIO 외부 인터럽트 관련 코드 완료

--- YU_Practice/project/src/main_gpio_int.c
+++ YU_Practice/project/src/main_gpio_int.c
@@ -22,7 +22,7 @@
 
 /*
 * 1. GPIO 버튼 초기화 한다. 
-*     GPIO 버튼은 GPIOA-0과 GPIOC-13이다. Input과 interrupt rising edge로 설정한다.
+*     GPIO 버튼은 GPIOA-0과 GPIOC-13이다. Input과 interrupt falling edge로 설정한다.
 * 2. GPIO RGB LED 초기화 한다.
 *     GPIO RGB는 각각 아래와 같다.
 *     RED -> GPIOB-10
@@ -37,7 +37,7 @@
 uint8_t key1, key2;
 void EXINT0_IRQHandler(void)
 {
-		if(gpio_input_data_bit_read(GPIOA, GPIO_PINS_0) == SET)
+		if(gpio_input_data_bit_read(GPIOA, GPIO_PINS_0) == RESET)
 		{
 			key1++;
 			key1 = key1%2;
@@ -48,8 +48,9 @@ void EXINT0_IRQHandler(void)
 
 void EXINT15_10_IRQHandler(void)
 {
-	if(gpio_input_data_bit_read(GPIOC, GPIO_PINS_13) == SET)
+	if(gpio_input_data_bit_read(GPIOC, GPIO_PINS_13) == RESET)
 	{
+
 		key2++;
 		key2 = key2%2;
 		exint_flag_clear(EXINT_LINE_13);
@@ -85,38 +86,24 @@ int main(void)
 	crm_periph_clock_enable(CRM_GPIOB_PERIPH_CLOCK, TRUE);
 	crm_periph_clock_enable(CRM_GPIOC_PERIPH_CLOCK, TRUE);
 	crm_periph_clock_enable(CRM_GPIOD_PERIPH_CLOCK, TRUE);
+	crm_periph_clock_enable(CRM_SCFG_PERIPH_CLOCK, TRUE);
 	
-	//인터럽트 설정
-	scfg_exint_line_config(SCFG_PORT_SOURCE_GPIOA, SCFG_PINS_SOURCE0);
-	scfg_exint_line_config(SCFG_PORT_SOURCE_GPIOC,SCFG_PINS_SOURCE13);
-	
-	//구조체 초기화
-	exint_default_para_init(&gpio_exint_struct);
-	
-	//외부 인터럽트 초기화
-	//exint 0번 부터 초기화
-	gpio_exint_struct.line_enable = TRUE;
-	gpio_exint_struct.line_mode = EXINT_LINE_INTERRUPT;
-	gpio_exint_struct.line_select = EXINT_LINE_0;
-	gpio_exint_struct.line_polarity = EXINT_TRIGGER_FALLING_EDGE;
-	exint_init(&gpio_exint_struct);
-	
-	nvic_priority_group_config(NVIC_PRIORITY_GROUP_0);
-	nvic_irq_enable(EXINT0_IRQn, 0, 0);
-	
-	
-	//exint 13번 초기화
+	//GPIO input 설정(스위치)
+	//GPIO A 스위치 핀 0번 input 처리
 	/*
-	gpio_exint_struct.line_select = EXINT_LINE_13;
+	gpio_init_struct.gpio_drive_strength = GPIO_DRIVE_STRENGTH_STRONGER;
+	gpio_init_struct.gpio_mode = GPIO_MODE_INPUT;
+	gpio_init_struct.gpio_out_type = GPIO_OUTPUT_PUSH_PULL;
+	gpio_init_struct.gpio_pins = GPIO_PINS_0;
+	gpio_init_struct.gpio_pull = GPIO_PULL_NONE;
 	
-	exint_init(&gpio_exint_struct);
+	gpio_init(GPIOA, &gpio_init_struct);
 	
-	nvic_priority_group_config(NVIC_PRIORITY_GROUP_0);
-	nvic_irq_enable(EXINT15_10_IRQn,0,0);
+	//GPIO C 스위치 핀 13번 input 처리
+	gpio_init_struct.gpio_pins = GPIO_PINS_13;
+	
+	gpio_init(GPIOD, &gpio_init_struct);
 	*/
-	
-	
-	
 	//포트 초기화
 	//LED 부터
 	//빨강, 파랑 초기화
@@ -129,33 +116,44 @@ int main(void)
 	gpio_init(GPIOB, &gpio_init_struct);
 	
 	//초록 초기화
-	//gpio_init_struct.gpio_pins = GPIO_PINS_13;
-	//gpio_init(GPIOD, &gpio_init_struct);
-	
-	/*
-	//Key1 초기화
-	gpio_init_struct.gpio_drive_strength = GPIO_DRIVE_STRENGTH_STRONGER;
-	gpio_init_struct.gpio_mode = GPIO_MODE_INPUT;
-	gpio_init_struct.gpio_out_type = GPIO_OUTPUT_PUSH_PULL;
-	gpio_init_struct.gpio_pins = (GPIO_PINS_0);
-	gpio_init_struct.gpio_pull = GPIO_PULL_NONE;
-	
-	gpio_init(GPIOA,&gpio_init_struct);
-	
-	
-	//key 2 초기화
 	gpio_init_struct.gpio_pins = GPIO_PINS_13;
-	gpio_init(GPIOC,&gpio_init_struct);
+	gpio_init(GPIOD, &gpio_init_struct);
 	
-	*/
+	
+	//인터럽트 설정
+	scfg_exint_line_config(SCFG_PORT_SOURCE_GPIOA, SCFG_PINS_SOURCE0);
+	scfg_exint_line_config(SCFG_PORT_SOURCE_GPIOC,SCFG_PINS_SOURCE13);
+	
+	//구조체 초기화
+	exint_default_para_init(&gpio_exint_struct);
+	
+	//외부 인터럽트 초기화
+	//exint 0번 부터 초기화
+	
+	gpio_exint_struct.line_enable = TRUE;
+	gpio_exint_struct.line_mode = EXINT_LINE_INTERRUPT;
+	gpio_exint_struct.line_select = EXINT_LINE_0;
+	gpio_exint_struct.line_polarity = EXINT_TRIGGER_FALLING_EDGE;
+	exint_init(&gpio_exint_struct);
+	
+	nvic_irq_enable(EXINT0_IRQn, 0, 0);
+	
+	//exint 13번 초기화
+	gpio_exint_struct.line_select = EXINT_LINE_13;
+	exint_init(&gpio_exint_struct);
+	
+	nvic_priority_group_config(NVIC_PRIORITY_GROUP_0);
+	nvic_irq_enable(EXINT15_10_IRQn,0,1);
 	
 	//LED는 sink이므로 set과 reset이 반대임!
 	
  // uint8_t key1, key2;
+ 
 	gpio_bits_set(GPIOB,GPIO_PINS_10);
 	gpio_bits_set(GPIOB,GPIO_PINS_5);
-	//gpio_bits_set(GPIOD,GPIO_PINS_13);
-  while(1)
+	gpio_bits_set(GPIOD,GPIO_PINS_13);
+  
+	while(1)
   {
 		
 		if((key1 == 0)&&(key2 == 0))
@@ -163,29 +161,31 @@ int main(void)
 			//모두 꺼짐
 			gpio_bits_set(GPIOB,GPIO_PINS_10);
 			gpio_bits_set(GPIOB,GPIO_PINS_5);
-			//gpio_bits_set(GPIOD,GPIO_PINS_13);
+			gpio_bits_set(GPIOD,GPIO_PINS_13);
+			
 		}
 		else if((key1 == 1)&&(key2 == 0))
 		{
 			//빨강 켜짐
 			gpio_bits_reset(GPIOB,GPIO_PINS_10);
 			gpio_bits_set(GPIOB,GPIO_PINS_5);
-			//gpio_bits_set(GPIOD,GPIO_PINS_13);
+			gpio_bits_set(GPIOD,GPIO_PINS_13);
 		}
-		else if((key1 == 1) && (key2 == 1))
+		else if((key1 == 0) && (key2 == 1))
 		{
 			//초록 켜짐
-			gpio_bits_reset(GPIOB,GPIO_PINS_10);
+			gpio_bits_reset(GPIOD,GPIO_PINS_13);
+			gpio_bits_set(GPIOB,GPIO_PINS_10);
 			gpio_bits_set(GPIOB,GPIO_PINS_5);
-			//gpio_bits_reset(GPIOD,GPIO_PINS_13);
 		}
 		else
 		{
 			//파랑 켜짐
-			gpio_bits_reset(GPIOB,GPIO_PINS_10);
-			gpio_bits_reset(GPIOB,GPIO_PINS_5);
-			//gpio_bits_set(GPIOD,GPIO_PINS_13);
+			gpio_bits_reset(GPIOB, GPIO_PINS_5);
+			gpio_bits_set(GPIOB,GPIO_PINS_10);
+			gpio_bits_set(GPIOD,GPIO_PINS_13);
 		}
+		
 		
 		//LED 초기화 잘 됐는지 테스트 용
 		/*
